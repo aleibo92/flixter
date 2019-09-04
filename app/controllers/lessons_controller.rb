36e99f6dff31f4ac851ccdd1f11a9f661f4c4ee4
aleibo92 <aleibo92@gmail.com>
2019-09-04 05:49:15 +0000

    lesson 22 carrierwave :)

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,18 +1,15 @@
 class LessonsController < ApplicationController
 
   def show
-   
   end
-
 
   private
 
   helper_method :current_lesson
-
-  def current_method
-    @current_method ||= Lesson.find(params[:id])
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
   end
-
-
-
 end
+
+
+
