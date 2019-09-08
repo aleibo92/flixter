be574a6c6931f06207e8c20a1297bf0f4b696fec
aleibo92 <aleibo92@gmail.com>
2019-09-08 02:31:53 +0000

    lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
+  
 
   def show
   end
@@ -6,6 +9,14 @@ class LessonsController < ApplicationController
   private
 
   helper_method :current_lesson
+  
+
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(@course) == false
+      redirect_to course_path(current_lesson.section.course), alert: 'You must be enrolled to view this page'
+    end
+  end
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
